d2367188625dcc3faf1bccbbbc35d8c08f68e006
Jacob Verburg <smucker45@hotmail.com>
2023-05-07 12:03:38 -0700

    see the readme for the commands used in chatgpt to create this project

---
+++ READEME.md
@@ -0,0 +1,8 @@
+
+Prompt used in chatGPT:
+
+Write me a BMI web app using Flask.
+
+Apply some styling to the webpage.  It looks very basic like in the mid 90s.
+
+Rewrite the program using imperial units.
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    weight = float(request.form['weight'])
+    height_feet = float(request.form['height_feet'])
+    height_inches = float(request.form['height_inches'])
+    height = (height_feet * 12 + height_inches) * 0.0254
+    bmi = weight / (height ** 2)
+    return render_template('result.html', bmi=bmi)
+
+if __name__ == '__main__':
+    app.run(debug=True)
